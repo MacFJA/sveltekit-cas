f7ddd82736e5c766e9b3ef19d3102e67eb11584d
MacFJA <macfja@gmail.com>
2023-01-10 23:17:08 +0100

    Change redirection + tailing slash + xml namespace

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,11 +7,23 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.0.2]
+
+### Fixed
+
+- Change permanente redirection to temporary redirection
+- Remove namespace in XML received from the CAS ([Issue#1])
+- Handle response that are not an XML from the CAS ([Issue#1])
+
+### Added
+
+- Add a new parameter to handle trailing slash in URL ([Issue#1])
+
 ## [2.0.1]
 
 ### Fixed
 
-- Fix import failure with CommonJS
+- Fix import failure with CommonJS ([Issue#1])
 
 ## [2.0.0]
 
@@ -31,8 +43,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 First version
 
-[unreleased]: https://github.com/MacFJA/sveltekit-cas/compare/2.0.1...HEAD
+[unreleased]: https://github.com/MacFJA/sveltekit-cas/compare/2.0.2...HEAD
+[2.0.2]: https://github.com/MacFJA/sveltekit-cas/releases/tag/2.0.2
 [2.0.1]: https://github.com/MacFJA/sveltekit-cas/releases/tag/2.0.1
 [2.0.0]: https://github.com/MacFJA/sveltekit-cas/releases/tag/2.0.0
 [1.0.1]: https://github.com/MacFJA/sveltekit-cas/releases/tag/1.0.1
 [1.0.0]: https://github.com/MacFJA/sveltekit-cas/releases/tag/1.0.0
+[issue#1]: https://github.com/MacFJA/sveltekit-cas/issues/1
--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,9 @@ type ServiceValidationSuccess = {
 	}
 	attributes: Record<string, string>
 }
+export type TrailingSlashFunction = (
+	route: Pick<RequestEvent, "route">["route"]
+) => "never" | "always" | "ignore"
 export type CasSession = {
 	/** The username of the current authenticated user. */
 	user: string
@@ -27,7 +30,12 @@ export type CasSession = {
 	attributes: Record<string, unknown>
 }
 
-const getCasHelper = (root: string, version: 1 | 2 | 3, event: RequestEvent) => {
+const getCasHelper = (
+	root: string,
+	version: 1 | 2 | 3,
+	event: RequestEvent,
+	trailing: TrailingSlashFunction
+) => {
 	return {
 		getValidationUrl(ticket: string): string {
 			let validationPath = ""
@@ -53,12 +61,21 @@ const getCasHelper = (root: string, version: 1 | 2 | 3, event: RequestEvent) => 
 			if (newUrl.pathname.endsWith("/__data.json")) {
 				newUrl.pathname = newUrl.pathname.substring(
 					0,
-					newUrl.pathname.length - "/__data.json".length
+					newUrl.pathname.length - "__data.json".length
 				)
 			}
 			newUrl.searchParams.delete("ticket")
 			newUrl.searchParams.delete("x-sveltekit-invalidated")
-			return newUrl.toString()
+			const urlString = newUrl.toString()
+
+			switch (trailing(event.route)) {
+				case "ignore":
+					return urlString
+				case "never":
+					return urlString.substring(0, urlString.length - (urlString.endsWith("/") ? 1 : 0))
+				case "always":
+					return urlString + (urlString.endsWith("/") ? "" : "/")
+			}
 		}
 	}
 }
@@ -69,7 +86,7 @@ function getTicket(event: RequestEvent): string | undefined {
 }
 function redirect(url: string): Response {
 	return new Response(null, {
-		status: 301,
+		status: 302,
 		headers: {
 			location: url
 		}
@@ -81,16 +98,17 @@ function redirect(url: string): Response {
  * @example
  * ```typescript
  * // src/hooks.server.ts
- * import { serverHook } from "@macfja/sveltekit-session";
+ * import { sessionHook } from "@macfja/sveltekit-session";
  * import { casHandler } from "@macfja/sveltekit-cas"
  * import type { Handle } from "@sveltejs/kit"
  * import { sequence } from "@sveltejs/kit/hooks"
  *
- * export const handle: Handle = sequence(serverHook, casHandler(
+ * export const handle: Handle = sequence(sessionHook(), casHandler(
  * 	 'http://0.0.0.0:8080',
  * 	 2,
  * 	 (event) => event.url.pathname.startsWith('/sverdle'),
- * 	 () => false
+ * 	 () => false,
+ * 	 (route) => route.id === '/blog/[slug]' ? 'never' : 'ignore'
  * ))
  * ```
  * @param casRoot
@@ -103,7 +121,9 @@ function redirect(url: string): Response {
  * @param rejectAccess
  * 		Define if for a request (first parameter) the authentication of a user (second parameter) is allowed (return true if the user is not allowed).
  * 		By default, there are no restriction on the authenticated user.
- *
+ * @param trailingSlash
+ * 		Define if the trailing slash of a route should be added/removed or left untouched
+ * 		By default, the trailing slash is left untouched
  * @return {Handle}
  */
 export function casHandler(
@@ -111,7 +131,8 @@ export function casHandler(
 	casVersion: 1 | 2 | 3,
 	authRequired: (event: RequestEvent) => boolean = () => true,
 	rejectAccess: (event: RequestEvent, authenticationInformation: CasSession) => boolean = () =>
-		false
+		false,
+	trailingSlash: TrailingSlashFunction = () => "ignore"
 ): Handle {
 	return function (input: Parameters<Handle>[0]): ReturnType<Handle> {
 		if (!authRequired(input.event)) {
@@ -131,18 +152,22 @@ export function casHandler(
 
 		switch (casVersion) {
 			case 1:
-				return cas1Hook(casRoot, input.event)
+				return cas1Hook(casRoot, input.event, trailingSlash)
 			case 2:
-				return cas23Hook(casRoot, 2, input.event)
+				return cas23Hook(casRoot, 2, input.event, trailingSlash)
 			case 3:
 			default:
-				return cas23Hook(casRoot, 3, input.event)
+				return cas23Hook(casRoot, 3, input.event, trailingSlash)
 		}
 	}
 }
 
-async function cas1Hook(casRoot: string, event: RequestEvent): Promise<Response> {
-	const casHelper = getCasHelper(casRoot, 1, event)
+async function cas1Hook(
+	casRoot: string,
+	event: RequestEvent,
+	trailing: TrailingSlashFunction
+): Promise<Response> {
+	const casHelper = getCasHelper(casRoot, 1, event, trailing)
 	let ticket: undefined | string
 	if ((ticket = getTicket(event)) !== undefined) {
 		const validation: Array<string> = await event
@@ -169,14 +194,31 @@ async function cas1Hook(casRoot: string, event: RequestEvent): Promise<Response>
 
 	return redirect(casHelper.getLoginUrl())
 }
-async function cas23Hook(casRoot: string, version: 2 | 3, event: RequestEvent): Promise<Response> {
-	const casHelper = getCasHelper(casRoot, version, event)
+async function cas23Hook(
+	casRoot: string,
+	version: 2 | 3,
+	event: RequestEvent,
+	trailing: TrailingSlashFunction
+): Promise<Response> {
+	const casHelper = getCasHelper(casRoot, version, event, trailing)
 	let ticket: undefined | string
+	// Forcefully remove namespace in XML node element
+	const namespaceRemover = (name: string) => (/:/.test(name) ? name.split(":")[1] : name)
 	if ((ticket = getTicket(event)) !== undefined) {
 		const validation: ServiceValidation = await event
 			.fetch(casHelper.getValidationUrl(ticket))
 			.then((response) => response.text())
-			.then((response) => xml2js(response, { compact: true }) as ServiceValidation)
+			.then(
+				(response) =>
+					xml2js(response, { compact: true, elementNameFn: namespaceRemover }) as ServiceValidation
+			)
+			.catch((reason) => {
+				console.error(
+					"An error occur while validating the CAS ticket: " + reason,
+					casHelper.getValidationUrl(ticket as string)
+				)
+				return {} as ServiceValidation
+			})
 
 		if (!Object.keys(validation).includes("serviceResponse")) {
 			console.error(
